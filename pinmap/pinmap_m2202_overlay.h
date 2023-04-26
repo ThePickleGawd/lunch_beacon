ca820ce8b8166ce3da3ec5fe3b60d238eca6598e
Dylan Lu <dylanelu@gmail.com>
2023-04-25 21:33:46 -0700

    Revert "sat scores :)" Forgot that we actually need the pinmap
    This reverts commit 972bd84dec3b774f14960071b9e431232a8ae518.

---
+++ pinmap/pinmap_m2202_overlay.h
@@ -0,0 +1,9 @@
+/*
+ * This file should be auto-generated using a tool that consumes the pin
+ * selections in the JSON file
+ */
+
+#define PIN_BUTTON1_IO 9
+#define PIN_BUTTON2_IO 10
+#define PIN_BUTTON3_IO 16
+#define PIN_BUTTON4_IO 18
---
+++ pinmap/pinmap_m3201_overlay.h
@@ -0,0 +1,7 @@
+/*
+ * This file should be auto-generated using a tool that consumes the pin
+ * selections in the JSON file
+ */
+
+#define PIN_BUTTON1_IO 9
+#define PIN_BUTTON2_IO 10
